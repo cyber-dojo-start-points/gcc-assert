c4cd037482e67635e5ad4eeb486972821b535ca9
Jon Jagger <jon@jaggersoft.com>
2020-06-15 11:44:13 +0100

    Renaming

--- run_tests.sh
+++ run_tests.sh
@@ -5,16 +5,16 @@ readonly TMP_DIR="$(mktemp -d ~/tmp.cyber-dojo-start-points.XXXXXXX)"
 remove_tmp_dir() { rm -rf "${TMP_DIR}" > /dev/null; }
 trap remove_tmp_dir INT EXIT
 
-run_tests()
+red_amber_green_test()
 {
-  local -r name=start_point_test.sh
+  local -r name=red_amber_green_test.sh
   if [ -x "$(command -v ${name})" ]; then
-    >&2 echo "Found executable ${name} on the PATH"
+    >&2 echo "Found ${name} on the PATH"
     echo "${name}"
   else
     local -r github=raw.githubusercontent.com
-    local -r org=cyber-dojo-tools
-    local -r repo=image_builder
+    local -r org=cyber-dojo-start-points
+    local -r repo=shared-scripts
     local -r branch=master
     local -r url="https://${github}/${org}/${repo}/${branch}/${name}"
     >&2 echo "Did not find executable ${name} on the PATH"
@@ -25,4 +25,4 @@ run_tests()
   fi
 }
 
-"$(run_tests)" "${1:-${MY_DIR}}"
+"$(red_amber_green_test)" "${1:-${MY_DIR}}"
